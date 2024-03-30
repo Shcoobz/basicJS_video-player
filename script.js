55839a63753afea93a0cf2cae48fa4fc178ca78f
Shcoobz <a.boeckmann23@gmail.com>
2024-03-30 11:46:38 +0100

    added volume slider functionality

--- script.js
+++ script.js
@@ -68,6 +68,35 @@ function setProgress(e) {
 
 // Volume Controls --------------------------- //
 
+// Volume Bar
+function changeVolume(e) {
+  let volume = e.offsetX / volumeRange.offsetWidth;
+  // Rounding volume up or down
+
+  if (volume < 0.1) {
+    volume = 0;
+  }
+
+  if (volume > 0.9) {
+    volume = 1;
+  }
+
+  volumeBar.style.width = `${volume * 100}%`;
+  video.volume = volume;
+
+  // Change icon depending on volume
+  volumeIcon.className = '';
+  if (volume > 0.5) {
+    volumeIcon.classList.add('fas', 'fa-volume-up');
+  } else if (volume <= 0.5 && volume > 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-down');
+  } else if (volume === 0) {
+    volumeIcon.classList.add('fas', 'fa-volume-off');
+  }
+
+  console.log(volume);
+}
+
 // Change Playback Speed -------------------- //
 
 // Fullscreen ------------------------------- //
@@ -78,3 +107,4 @@ video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
 progressRange.addEventListener('click', setProgress);
+volumeRange.addEventListener('click', changeVolume);
